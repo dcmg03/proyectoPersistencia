0916e7514ee4a84d9fc27bef4b4af7a3f5cb1172
Deisy Carolina Monroy Gutiérrez <86481741+dcmg03@users.noreply.github.com>
2021-10-02 21:32:40 -0500

    new

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.proyectopersistencia {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.graphics;
+
+
+    opens com.example.proyectopersistencia to javafx.fxml;
+    exports com.example.proyectopersistencia;
+}
