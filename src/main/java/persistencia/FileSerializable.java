82d37d07b09c8592d57d925860e921f583b58894
Karen Hernández <karen.hernandez06@uptc.edu.co>
2021-10-09 17:55:02 -0500

    9/10/2021

---
+++ src/main/java/persistencia/FileSerializable.java
@@ -0,0 +1,50 @@
+package persistencia;
+
+import java.io.*;
+
+public class FileSerializable implements Serializable {
+
+        private String path;
+        private String name;
+        private File file;
+
+        public String getPath() {
+            return path;
+        }
+
+        public void setPath(String path) {
+            this.path = path;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void openFile(){
+            file = new File( path + name );
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public Object readObject() throws IOException, ClassNotFoundException {
+            ObjectInputStream input = new ObjectInputStream( new FileInputStream( file ) );
+            Object object = input.readObject();
+            input.close();
+
+            return object;
+        }
+
+        public void writeObject( Object object ) throws IOException {
+            ObjectOutputStream output = new ObjectOutputStream( new FileOutputStream( file ) );
+            output.writeObject( object );
+
+            output.close();
+        }
+}
+
