3d9f6c3c554267d967ec79ad8186488d9529cb08
Deisy Carolina Monroy Gutiérrez <86481741+dcmg03@users.noreply.github.com>
2021-10-12 10:20:28 -0500

    diagrama

--- src/main/java/com/example/proyectopersistencia/HelloController.java
+++ src/main/java/com/example/proyectopersistencia/HelloController.java
@@ -9,7 +9,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import logica.Domiciliario;
